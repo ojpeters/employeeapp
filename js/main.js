194c15429e7fd445d6bbbed1dbbb7dd0156f24b2
ojpeters <jeze@J2XN0113150.wkstn.toronto.ca>
2015-08-30 17:16:32 -0400

    Added API contacts

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 var app = {
 	
-   
+	route: function() {
+		var hash = window.location.hash;
+			if (!hash) {
+				$('body').html(new HomeView(this.store).render().el);
+				return;
+			}
+			var match = hash.match(app.detailsURL);
+			if (match) {
+				this.store.findById(Number(match[1]), function(employee) {
+				$('body').html(new EmployeeView(employee).render().el);
+				});
+			}
+	},
+
+
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -9,7 +23,8 @@ var app = {
 		}
 	},
 	registerEvents: function() {    
-		var self = this;    // Check of browser supports touch events...   
+		var self = this;    // Check of browser supports touch events... 
+		$(window).on('hashchange', $.proxy(this.route, this));
 	 if (document.documentElement.hasOwnProperty('ontouchstart')) {        // ... if yes: register touch event listener to change the "selected" state of the item     
 		   $('body').on('touchstart', 'a', function(event) {            
 		   $(event.target).addClass('tappable-active');     
@@ -29,10 +44,12 @@ var app = {
 	 
     initialize: function() {
 		var self = this;
+		this.detailsURL = /^#employees\/(\d{1,})/;
 		self.registerEvents() ;
         this.store = new WebSqlStore(
 			function() { 
-				$('body').html(new HomeView(self.store).render().el);
+				//$('body').html(new HomeView(self.store).render().el);
+				self.route();
 				self.showAlert('DBStore Initialized', 'Info');    
 			}
 		);
